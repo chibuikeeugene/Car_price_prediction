fab463d56c990449a124146a32555b555ed3f546
chibuikeeugene <chibuikeeugene@gmail.com>
2023-06-18 00:23:40 +0100

    deploying locally car-price-api

---
+++ model-serving-api/car-prices-api/app/schemas/predict.py
@@ -0,0 +1,40 @@
+from typing import Any, List, Optional
+
+from regression_model.processing.validation import CarDataInputSchema
+from pydantic import BaseModel
+
+
+class PredictionResults(BaseModel):
+    errors: Optional[Any]
+    version: str
+    predictions: Optional[List[float]]
+
+
+class MultipleCarDataInputs(BaseModel):
+    inputs: List[CarDataInputSchema]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "inputs": [
+                    {
+                        "make": "BMW",
+                        "model": "1 Series M",
+                        "year": 2011,
+                        "engine_fuel_type": "premium unleaded (required)",
+                        "engine_hp": 335.0,
+                        "engine_cylinders": 6.0,
+                        "transmission_type": "MANUAL",
+                        "driven_wheels": "rear wheel drive",
+                        "number_of_doors": 2.0,
+                        "market_category": "Factory Tuner,Luxury,High-Performance",
+                        "vehicle_size": "Compact",
+                        "vehicle_style": "Coupe",
+                        "highway_mpg": 26,
+                        "city_mpg": 19,
+                        "popularity": 3916,
+                        "msrp": 46135,
+                    }
+                ]
+            }
+        }
